be58a1a34b25320b7a4294455c4dca571e789628
MegaaMaax <mxm.vaultier@gmail.com>
2024-09-18 10:32:15 +0200

    [ADD] vector database and similarity search with Chroma db

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ import sys
 import fitz
 import gradio as gr
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+vector_store = Chroma(
+    collection_name="rag_db",
+    embedding_function=OllamaEmbeddings(model='nomic-embed-text'),
+    persist_directory="db_dir",
+)
+
+def upload_database(file):
+    doc = fitz.open(file.name)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_text(text)
+    vector_store.add_texts(texts=splits)
+    return "Database uploaded successfully"
 
 # Obtenir la liste des modèles disponibles
 def update_name_list():
@@ -56,34 +72,40 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 # Fonction pour poser une question à l'IA
-def ask_question(question, model, file):
+def ask_question(question, model, file, check_db):
     global conversation_history
 
+    if check_db:
+        print("Début search in db")
+        formatted_context = ""
+        results = vector_store.similarity_search(query=question, k=5)
+        for res in results:
+            formatted_context += f"{res.page_content}\n"
+        formatted_prompt = f"Question: {question}\n\nContext from database: {formatted_context}"
+        print("Recherche dans la base de données effectuée")
+    else:
+        formatted_prompt = f"Question: {question}"
+
     if file is not None:
-        print("debut rag chain")
+        print("Début RAG chain")
         retriever = load_and_retrieve_docs_from_pdf(file)
         retrieved_docs = retriever.invoke(question)
         formatted_context = format_docs(retrieved_docs)
-        formatted_prompt = f"Question: {question}\n\nContext: {formatted_context}"
-        print("embedding crée")
-        stream = ollama.chat(model=model, messages=[{'role': 'user', 'content': formatted_prompt}], stream=True)
-        response = ""
-        for chunk in stream:
-            response += chunk['message']['content']
-            yield response
-    else:
-        conversation_history.append({'role': 'user', 'content': question})
-        print("The model selected is " + model)
-        stream = ollama.chat(
-            model=model,
-            messages=conversation_history,
-            stream=True,
-        )
-        response = ""
-        for chunk in stream:
-            response += chunk['message']['content']
-            yield response
+        formatted_prompt += f"\n\nContext from PDF: {formatted_context}"
+        print("Embedding créé")
+    
+    stream = ollama.chat(model=model, messages=[{'role': 'user', 'content': formatted_prompt}], stream=True)
+    response = ""
+    for chunk in stream:
+        response += chunk['message']['content']
+        yield response
+
+    conversation_history.append({'role': 'user', 'content': question})
     conversation_history.append({'role': 'assistant', 'content': response})
+
+def handle_create_model(base_name, new_name, parameter):
+    new_model_names = create_custom_model(base_name, new_name, parameter)
+    return gr.update(choices=new_model_names), gr.update(choices=new_model_names)
 
 # Fonction pour créer un modèle personnalisé
 def create_custom_model(base_name, new_name, parameter):
@@ -98,37 +120,51 @@ def create_custom_model(base_name, new_name, parameter):
 with gr.Blocks(theme='gradio/soft') as iface:
     gr.Markdown("# AI Chatbot")
 
-    with gr.Row():
-        with gr.Column():
-            question = gr.Textbox(label="Question")
-            model = gr.Dropdown(label="Model", choices=model_names, value="tinyllama")
-            file = gr.File(label="Upload PDF file", file_types=["pdf"])
-            submit_button = gr.Button("Submit")
-            
-            with gr.Accordion("Customize Model", open=False):
-                base_model_name = gr.Dropdown(label="Base Model", choices=model_names, value="tinyllama")
-                new_model_name = gr.Textbox(label="New Model Name")
-                model_parameter = gr.Textbox(label="Model Parameter")
-                create_button = gr.Button("Create Model")
-            
-        with gr.Column():
-            output = gr.Textbox(label="Response")
+    with gr.Tab("Chatbot"):
+        with gr.Row():
+            with gr.Column():
+                question = gr.Textbox(label="Question")
+                model = gr.Dropdown(label="Model", choices=model_names, value="tinyllama")
+                check_db = gr.Checkbox(label="Use Database")
+                file = gr.File(label="Upload PDF file", file_types=["pdf"])
+                submit_button = gr.Button("Submit")
+                
+            with gr.Column():
+                output = gr.Textbox(label="Response")
 
-    def handle_create_model(base_name, new_name, parameter):
-        new_model_names = create_custom_model(base_name, new_name, parameter)
-        return gr.update(choices=new_model_names), gr.update(choices=new_model_names)
 
-    create_button.click(
-        fn=handle_create_model,
-        inputs=[base_model_name, new_model_name, model_parameter],
-        outputs=[model, base_model_name]
-    )
+        submit_button.click(
+            fn=ask_question,
+            inputs=[question, model, file, check_db],
+            outputs=output
+        )
 
-    submit_button.click(
-        fn=ask_question,
-        inputs=[question, model, file],
-        outputs=output
-    )
+    with gr.Tab("Customize Model"):
+        base_model_name = gr.Dropdown(label="Base Model", choices=model_names, value="tinyllama")
+        new_model_name = gr.Textbox(label="New Model Name")
+        model_parameter = gr.Textbox(label="Model Parameter")
+        create_button = gr.Button("Create Model")
+
+        create_button.click(
+            fn=handle_create_model,
+            inputs=[base_model_name, new_model_name, model_parameter],
+            outputs=[model, base_model_name]
+        )
+
+    with gr.Tab("Upload database"):
+        with gr.Row():
+            with gr.Column():
+                db_file = gr.File(label="Upload Database file", file_types=["pdf"])
+                submit_db_button = gr.Button("Submit")
+
+            with gr.Column():
+                db_output = gr.Textbox(label="Response")
+
+        submit_db_button.click(
+            fn=upload_database,
+            inputs=[db_file],
+            outputs=db_output
+        )
 
 # Lancer l'application
 iface.launch(server_name="0.0.0.0", server_port=8080)
