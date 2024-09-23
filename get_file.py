024d332b109efcc5b7697905832af05e67b2f47f
Maxime VAULTIER 755 <maxime.vaultier@cnaf.fr>
2024-09-23 13:44:27 +0200

    [ADD] file share

---
+++ get_file.py
@@ -0,0 +1,21 @@
+import socket
+
+def request_file(server_ip, port, filename):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, port))
+    client_socket.sendall(filename.encode())
+
+    with open('received_' + filename, 'wb') as file:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    server_ip = '192.168.1.2'  # Remplacez par l'IP du serveur
+    port = 12345
+    filename = input("Enter the filename to request: ")
+    request_file(server_ip, port, filename)
